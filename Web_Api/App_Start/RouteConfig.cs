ad0f826e38b2e1a0f76ac8ac20b354d158213a2b
mitschi11051994 <mitschi1994@hotmail.com>
2017-08-20 16:15:40 -0600

    Iniciar Proyecto

--- Web_Api/App_Start/RouteConfig.cs
+++ Web_Api/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace Web_Api
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Cliente",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Clientes", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
